0781d521f467fc5ba1d2ce6a2f270dc67e5958d1
Daniel Luong <danielluong@hotmail.ca>
2019-01-06 18:12:57 -0800

    card class update

--- src/p1/Card.java
+++ src/p1/Card.java
@@ -4,35 +4,55 @@
 package p1;
 
 /**
+ * A card in a deck of 52 Cards.
  * @author DDD
- *
+ * @version 1.0
  */
 public class Card {
+    /**
+     * Array of rank of cards.
+     */
+    private int[] rankList = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10, 11};
     
     /**
-     * Cards.
+     * Face value of the cards.
      */
-    private String[][] card = {
-        {"Spades", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"},
-        {"Diamonds", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"},
-        {"Hearts", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"},
-        {"Clubs", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"},
-    };
+    private String[] faceList = {"2", "3", "4", "5", "6", "7", 
+            "8", "9", "10", "Jack", "Queen", "King", "Ace"};
     
-    
-    
-    public String getCard() {
-        String result = "";
+    /**
+     * Array of suits.
+     */
+    private String[] suitList = {"Spades", "Clubs", "Diamonds", "Hearts"};
+    /**
+     * Suit of card.
+     */
+    private String suit;
+    /**
+     * Rank of card.
+     */
+    private int rank;
+    /**
+     * Value of card.
+     */
+    private int value;
+    /**
+     * Face of the card.
+     */
+    private String face;
+
+    /**
+     * A card in a 52 card deck.
+     * @param suitIndex int
+     * @param rankIndex int
+     */
+    public Card(int suitIndex, int rankIndex) {
+        suit = suitList[suitIndex];
+        rank = rankList[rankIndex];
+        value = rankList[rankIndex];
+        face = faceList[rankIndex];
         
-        
-        return result;
     }
     
-    @Override
-    public String toString() {
-        String result = "";
-        
-        
-        return result;
-    }
+    
 }
