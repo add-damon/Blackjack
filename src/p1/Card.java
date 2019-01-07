5919076972ef2465da7e74c7655c6def1474647d
damon ren <demiao.ren@gmail.com>
2019-01-06 17:36:38 -0800

    Eclipse setup

---
+++ src/p1/Card.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package p1;
+
+/**
+ * @author DDD
+ *
+ */
+public class Card {
+    
+    /**
+     * Cards.
+     */
+    private String[][] card = {
+        {"Spades", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"},
+        {"Diamonds", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"},
+        {"Hearts", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"},
+        {"Clubs", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"},
+    };
+    
+    
+    
+    public String getCard() {
+        String result = "";
+        
+        
+        return result;
+    }
+    
+    @Override
+    public String toString() {
+        String result = "";
+        
+        
+        return result;
+    }
+}
---
+++ src/p1/Driver.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package p1;
+
+/**
+<<<<<<< HEAD
+ * @author DDD
+=======
+ * @author damonren
+>>>>>>> refs/remotes/origin/master
+ *
+ */
+public class Driver {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
