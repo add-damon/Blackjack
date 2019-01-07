2fae3095b1f8563eb86bc1540ac9d93e14c8943b
damon ren <demiao.ren@gmail.com>
2019-01-07 10:54:19 -0800

    Modifies the Driver.java to test the other classes.

--- src/p1/Driver.java
+++ src/p1/Driver.java
@@ -3,6 +3,10 @@
  */
 package p1;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * @author 
  *
@@ -13,9 +17,36 @@ public class Driver {
      * @param args
      */
     public static void main(String[] args) {
-        Deck deck1 = new Deck();
-        System.out.println(deck1);
-
+        ArrayList<Player> players = new ArrayList<Player>();
+        int numOfPlayers = 0;
+        boolean valid;
+        Scanner scan = new Scanner(System.in);
+        do {
+            System.out.print("How many players: ");
+            try {
+                numOfPlayers = scan.nextInt();
+                valid = true;
+            } catch (InputMismatchException e) {
+                scan.nextLine();
+                System.out.println(e + ": Please enter an integer");
+                valid = false;
+            }
+        } while (!valid);
+        
+        Deck d = new Deck();
+        
+        //the first player will be the dealer.
+        for (int counter = 0; counter <= numOfPlayers; counter ++) {
+            Player newPlayer = new Player();
+            newPlayer.hit(d);
+            newPlayer.hit(d);
+            players.add(new Player());
+            System.out.println("Player" + counter + ": \n" + newPlayer);
+        }
+        
+        
+        scan.close();
+        System.out.println("Game Ended.");
     }
 
 }
