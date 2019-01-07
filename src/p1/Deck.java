45859ed253a156d2716531dcba3dad2e646c6955
damon ren <demiao.ren@gmail.com>
2019-01-06 18:52:02 -0800

    adds constructor in Deck.java

--- src/p1/Deck.java
+++ src/p1/Deck.java
@@ -1,27 +1,36 @@
 
 package p1;
 
+import java.util.ArrayList;
 
 public class Deck {
     
     private final int numOfCardsInADeck = 52;
     private final int numOfSuits = 4;
-    private Card[] deck;
+    private final int numOfRanks = 13;
+    private ArrayList<Card> deck;
     
     public Deck () {
-        deck = new Card[numOfCardsInADeck];
+        deck = new ArrayList<Card>();
         for (int suitIndex = 0; suitIndex < numOfSuits; suitIndex++) {
-            
+            for (int rankIndex = 0; rankIndex < numOfRanks; rankIndex++) {
+                deck.add(new Card(suitIndex, rankIndex));
+            }
         }
     }
     
-    public Card deal() {
-        
-    }
+//    public Card deal() {
+//        
+//    }
     
     public void shuffle() {
         
     }
     
     
+    public String toString() {
+        String result = "";
+        
+        return result;
+    }
 }
