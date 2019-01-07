a1a486e13e5cbadff015aec223150a0745068d97
damon ren <demiao.ren@gmail.com>
2019-01-07 10:53:50 -0800

    Adds constructor, hit() and stand() to Player.java

--- src/p1/Player.java
+++ src/p1/Player.java
@@ -1,12 +1,33 @@
 package p1;
 
+import java.util.ArrayList;
+
 public class Player {
-    private Card[] cardPile;
+    private ArrayList<Card> cardOnHands;
     
-    public void hit() {
-        
+    public Player() {
+        cardOnHands = new ArrayList<Card>();
     }
     
+    public void stand() {
+        return;
+    }
     
+    public void hit(Deck d) {
+        cardOnHands.add(d.deal());
+    }
+    
+    /**
+     * Returns a concatenation of cards on this player.
+     * @return String.
+     */
+    @Override
+    public String toString() {
+        String result = "";
+        for (Card c : cardOnHands) {
+            result += c + "\n";
+        }
+        return result;
+    }
 
 }
